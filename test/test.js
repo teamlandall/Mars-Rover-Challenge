ceaaf4beadf0ab16b6502592e9865f97002fc1ce
landallproctor <landallproctor@gmail.com>
2017-09-14 09:56:36 -0400

    Initital commit setting up folders and installing mocha for tests.

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+
+describe('Rover', function () {
+
+  const Rover = require('../src/rover');
+
+  describe('', function () {
+
+    it('', function () {
+
+      assert.equal('', Rover());
+    });
+
+    
+  });
+});
