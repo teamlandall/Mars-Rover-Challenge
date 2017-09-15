ec5e7d1e7898bd6bb9fd315b610a2692dd3ea386
landallproctor <landallproctor@gmail.com>
2017-09-15 05:53:59 -0400

    Updates tests and api response.

--- src/rover.js
+++ src/rover.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 /*
   Mars Rover Challenge. 
 
@@ -10,11 +12,117 @@
   - Implement wrapping from one edge of the grid to another. (planets are spheres after all)
   - Implement obstacle detection before each move to a new square. If a given sequence of commands encounters an obstacle, the rover   moves up to the last possible point and reports the obstacle.
 
+  @params 
+  pos = {
+    x: number, // x coordinate
+    y: number, // y coordinate
+    dir: oneOf[N,S,E,W] // cardinal directions
+  };
+  input = ["L", "L", "M", "M"] // takes L, R, M. L = left, R = right, M = move 1 space. 
+  obstacle = {
+    x: number, // x coordinate of obstacle
+    y: number, // y coordinate of obstacle
+  }
+
 */
 
-function rover() {
+function rover(pos, input, obstacle) {
+  if (!pos || _.isEmpty(pos)) return "No initial position provided";
+  if (!input || !input.length) return "No input provided";
+
+  for (var i = 0; i < input.length; i++) {
+    const changeDirection = !!(input[i] === "L" || input[i] === "R");
+    if (!pos) return;
+    if (changeDirection) {
+      pos.dir = turnRover(pos.dir, input[i]);
+    } else {
+      pos = (input[i] === "F") ? moveRoverForward(pos, obstacle) : moveRoverBackward(pos, obstacle);
+    }
+
+  }
+  console.log('final pos: ', pos);
+  return pos ? pos : "Obstacle detected. Unable to continue.";
 
 };
 
 module.exports = rover;
 
+//helper functions
+function turnRover(dir, turn) {
+
+ switch(dir) {
+   case "N":
+    return turn === "L" ? "W" : "E";
+    break;
+   case "S":
+    return turn === "L" ? "E" : "W";
+    break;
+   case "E":
+    return turn === "L" ? "N" : "S";
+    break;
+   case "W":
+    return turn === "L" ? "S" : "N";
+    break;
+ }
+ 
+};
+
+function moveRoverForward(pos, obstacle) {
+
+  switch (pos.dir) {
+    case "N":
+      pos.y++;
+      pos.y = pos.y > 10 ? 0 : pos.y;
+      break;
+    case "S":
+      pos.y--;
+      pos.y = pos.y < 0 ? 10 : pos.y;
+      break;
+    case "E":
+      pos.x++;
+      pos.x = pos.x > 10 ? 0 : pos.x;
+      break;
+    case "W":
+      pos.x--;
+      pos.x = pos.x < 0 ? 10 : pos.x;
+      break;
+  }
+
+  if (pos.x === obstacle.x && pos.y === obstacle.y) {
+    console.log('Obstacle in path. Unable to continue');
+    return; 
+  }
+
+  return pos;
+
+};
+
+function moveRoverBackward(pos, obstacle) {
+
+  switch (pos.dir) {
+    case "N":
+      pos.y--;
+      pos.y = pos.y < 0 ? 10 : pos.y;
+      break;
+    case "S":
+      pos.y++;
+      pos.y = pos.y > 10 ? 0 : pos.y;
+      break;
+    case "E":
+      pos.x--;
+      pos.x = pos.x < 0 ? 10 : pos.x;
+      break;
+    case "W":
+      pos.x++;
+      pos.x = pos.x > 10 ? 0 : pos.x;
+      break;
+  }
+
+  if (pos.x === obstacle.x && pos.y === obstacle.y) {
+    console.log('Obstacle in path. Unable to continue');
+    return;
+  }
+
+  return pos;
+
+};
--- test/test.js
+++ test/test.js
@@ -1,16 +1,127 @@
 var assert = require('assert');
 
-describe('Rover', function () {
+describe('Rover', () => {
 
   const Rover = require('../src/rover');
 
-  describe('', function () {
+  describe('validate params', () => {
 
-    it('', function () {
+    it('should throw error if no initialPos is provided', () => {
+      const pos = {};
+      const input = ["L", "L", "M"];
+      const obstacle = {x: 1, y: 2};
 
-      assert.equal('', Rover());
+      assert.equal('No initial position provided', Rover(pos, input, obstacle));
     });
 
+    it('should throw error if no input is provided', () => {
+      const pos = {x: 0, y: 0, dir: "N"};
+      const input = null;
+      const obstacle = { x: 1, y: 2 };
+
+      assert.equal('No input provided', Rover(pos, input, obstacle));
+    });
     
   });
+
+  describe('Check rovers navigation', () => {
+
+    it('should turn left', () => {
+      const pos = { x: 0, y: 0, dir: "N" };
+      const input = ["L"];
+      const obstacle = { x: 0, y: 6 };
+
+      const expectedResult = {x:0, y:0, dir: "W"};
+
+      assert.deepEqual(expectedResult, Rover(pos, input, obstacle));
+    });
+
+    it('should turn right', () => {
+      const pos = { x: 0, y: 0, dir: "N" };
+      const input = ["R"];
+      const obstacle = { x: 0, y: 6 };
+
+      const expectedResult = { x: 0, y: 0, dir: "E" };
+
+      assert.deepEqual(expectedResult, Rover(pos, input, obstacle));
+    });
+
+    it('should move forward', () => {
+      const pos = { x: 0, y: 0, dir: "N" };
+      const input = ["F"];
+      const obstacle = { x: 0, y: 6 };
+
+      const expectedResult = { x: 0, y: 1, dir: "N" };
+
+      assert.deepEqual(expectedResult, Rover(pos, input, obstacle));
+    });
+
+    it('should move backward', () => {
+      // change starting position, not testing grid wrapping yet
+      const pos = { x: 1, y: 1, dir: "N" };
+      const input = ["B"];
+      const obstacle = { x: 0, y: 6 };
+
+      const expectedResult = { x: 1, y: 0, dir: "N" };
+
+      assert.deepEqual(expectedResult, Rover(pos, input, obstacle));
+    });
+
+    describe('Check rovers grid wrapping', () => {
+
+      it('should wrap top to bottom', () => {
+        const pos = { x: 0, y: 10, dir: "N" };
+        const input = ["F"];
+        const obstacle = { x: 0, y: 6 };
+
+        const expectedResult = { x: 0, y: 0, dir: "N" };
+
+        assert.deepEqual(expectedResult, Rover(pos, input, obstacle));
+      });
+
+      it('should wrap bottom to top', () => {
+        const pos = { x: 0, y: 0, dir: "N" };
+        const input = ["B"];
+        const obstacle = { x: 0, y: 6 };
+
+        const expectedResult = { x: 0, y: 10, dir: "N" };
+
+        assert.deepEqual(expectedResult, Rover(pos, input, obstacle));
+      });
+
+      it('should wrap left to right', () => {
+        const pos = { x: 0, y: 0, dir: "N" };
+        const input = ["L", "F"];
+        const obstacle = { x: 0, y: 6 };
+
+        const expectedResult = { x: 10, y: 0, dir: "W" };
+
+        assert.deepEqual(expectedResult, Rover(pos, input, obstacle));
+      });
+
+      it('should wrap right to left', () => {
+        const pos = { x: 10, y: 0, dir: "N" };
+        const input = ["R", "F"];
+        const obstacle = { x: 0, y: 6 };
+
+        const expectedResult = { x: 0, y: 0, dir: "E" };
+
+        assert.deepEqual(expectedResult, Rover(pos, input, obstacle));
+      });
+    });
+
+    describe('check obstacle detection', () => {
+
+      it('should detect obstacle', () => {
+        const pos = { x: 0, y: 0, dir: "N" };
+        const input = ["F", "F"];
+        const obstacle = { x: 0, y: 2 };
+
+        const expectedResult = "Obstacle detected. Unable to continue.";
+        assert.equal(expectedResult, Rover(pos, input, obstacle));
+      })
+    });
+
+  });
+
 });
